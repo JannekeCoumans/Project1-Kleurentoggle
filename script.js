86e30ec8f09666fa1ed8dd5a0deb6b679a1d6476
Janneke Coumans <jannekecoumans@gmail.com>
2020-05-07 20:54:25 +0200

    script DRY gemaakt

--- script.js
+++ script.js
@@ -1,62 +1,30 @@
-const menuImg = document.getElementById('menuImg');
-const menu = document.getElementById('colorMenu')
-const bodyBg = document.body;
-const changeH1Text = document.getElementById('h1Text');
-
-// // Functies van openen en sluiten van menu
-const menuOpen = function(){
-    menu.setAttribute('style', 'height: 460px;');
-}
-
-const menuClose = function(){
-    menu.setAttribute('style', 'height: 0px;');
-}
-
-// Hier klapt het menu uit als ik over de menuknop hover.
-menuImg.addEventListener("mouseover", function(){
-    menuOpen();
-});
-
-menu.addEventListener("mouseleave", function(){
-    menuClose();
-});
-
-// Hier verandert #homeBtn de achtergrondkleur
-const homeBtn = document.getElementById('homeBtn');
-homeBtn.addEventListener("click", function(){
-    bodyBg.setAttribute('style', 'background-color: #FFF;');
-    menuClose();
-    changeH1Text.innerHTML = "Home";
+const menuImage = document.getElementById("menuImage");
+const menuContent = document.getElementById("colorMenu");
+menuImage.addEventListener("mouseover", () => {
+    menuContent.setAttribute("style", "display: block;")
 })
 
-// Hier verandert #redBtn de achtergrondkleur
-const redBtn = document.getElementById('redBtn');
-redBtn.addEventListener("click", function(){
-    bodyBg.setAttribute('style', 'background-color: #ed4743;');
-    menuClose();
-    changeH1Text.innerHTML = "Rood";
+menuContent.addEventListener("mouseleave", () => {
+    menuContent.setAttribute("style", "display: none")
 })
 
-// Hier verandert #greenBtn de achtergrondkleur
-const greenBtn = document.getElementById('greenBtn');
-greenBtn.addEventListener("click", function(){
-    bodyBg.setAttribute('style', 'background-color: #5cc266;');
-    menuClose();
-    changeH1Text.innerHTML = "Groen";
-})
+const colorsList = document.getElementById("colors");
+colors = [
+    {name: "whitesmoke", hex: "#f7f9f6"},
+    {name: "darkslategray", hex: "#3a5a55"},
+    {name: "lightcoral", hex: "#ea747d"},
+    {name: "darkseagreen", hex: "#81a28f"},
+    {name: "seagreen", hex: "#93c5a2"},
+]
 
-// Hier verandert #yellowBtn de achtergrondkleur
-const yellowBtn = document.getElementById('yellowBtn');
-yellowBtn.addEventListener("click", function(){
-    bodyBg.setAttribute('style', 'background-color: #e7c900;');
-    menuClose();
-    changeH1Text.innerHTML = "Geel";
-})
-
-// Hier verandert #blueBtn de achtergrondkleur
-const blueBtn = document.getElementById('blueBtn');
-blueBtn.addEventListener("click", function(){
-    bodyBg.setAttribute('style', 'background-color: #299ed3;');
-    menuClose();
-    changeH1Text.innerHTML = "Blauw";
+const menuItems = colors.forEach(color => {
+    const newLi = document.createElement('li');
+    const headerText = document.getElementById('h1Text');
+    newLi.innerHTML = color.name;
+    newLi.setAttribute("style", `background-color: ${color.hex}`);
+    colorsList.appendChild(newLi);
+    newLi.addEventListener("click", (event) => {
+        document.body.setAttribute("style", `background-color: ${color.hex}`)
+        headerText.innerHTML = color.name;
+    });
 })
